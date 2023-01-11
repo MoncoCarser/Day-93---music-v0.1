020fff3b7608550b1861b9cd292e0560d93bd3ab
Pasi P <pjpakkala@gmail.com>
2023-01-11 21:03:45 +0000

    template.html added, music.py and main.py updated to almost final form. Printing songs to web page now works.

--- main.py
+++ main.py
@@ -5,16 +5,41 @@ app = Flask(__name__)
 
 #print(get_songs())
 
+def print_song(year):
+    entry = ""
+    f = open("./templates/template.html", "r")
+    entry = f.read()
+    f.close
+    data = get_songs(year)
+    content = ""
+    for track in data["tracks"]["items"]:
+        this_entry = entry
+        this_entry = this_entry.replace("{name}", track["name"])
+        this_entry = this_entry.replace("{url}", track["preview_url"])
+        content += this_entry
+    return content
+    
+
 @app.route("/")
 def choose_year():
-    return render_template("form.html", year="")
-
+    page = ""
+    f = open("./templates/form.html", "r")
+    page = f.read()
+    f.close()
+    page = page.replace("{music_printout}", "")
+    return page
+    
 @app.route("/top_10_of_the_year", methods=["POST"])
 def music_page():
     form = request.form
-    year_to_search = form["year"]
+    page = ""
+    f = open("./templates/form.html", "r")
+    page = f.read()
+    f.close()
+    page = page.replace("{music_printout}", print_song(form["year"]))
+    return page
     
-    return render_template("form.html", year=get_songs(year_to_search)) #this isn't getting anything to website
+    #this isn't getting anything to website
 #if I save  the songs and links to a list, then print that list to a separate template? For example look Day 86 exercise 
 
 app.run(host="0.0.0.0", port=81 )
--- music.py
+++ music.py
@@ -21,20 +21,25 @@ def asking_for_songs_per_year(year):
 
 #for this project we use year to get 10 top songs of that year
   #  year = input("Year: ")
+
+    offset = 0
     
     url = "https://api.spotify.com/v1/search"
     headers = {"Authorization": f"Bearer {access_token}"}
     #search = f"?q=artist%3A{artist}&type=track&limit=5"
-    search = f"?q=year%3A{year}&type=track&limit=10"
+    search = f"?q=year%3A{year}&type=track&limit=10&offset={offset}"
     
     full_url = f"{url}{search}"
     
     response = requests.get(full_url, headers=headers)
     data = response.json()
 
+    return data
+
     #with this we can show the whole response in clearest form
     #print(json.dumps(data, indent=2))
-    
-    for track in data["tracks"]["items"]:
+
+    #to print only the song name and preview url:
+    """for track in data["tracks"]["items"]:
         print(track["name"])
-        print(track["preview_url"])                        
+        print(track["preview_url"]) """                   
