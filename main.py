7e69a7883475ea32316064131263d8a6bbe6b8f9
Pasi P <pjpakkala@gmail.com>
2023-01-12 05:57:59 +0000

    *Artist {name} is now shown by updating template.html and main.py *main.py was cleaned to only have the web page details *music.py holds both functions to get the data and print it out *form.html: input box was updated to be meant for text, with min and max years.

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from music import asking_for_songs_per_year as get_songs
+from music import print_song
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-#print(get_songs())
-
-def print_song(year):
-    entry = ""
-    f = open("./templates/template.html", "r")
-    entry = f.read()
-    f.close
-    data = get_songs(year)
-    content = ""
-    for track in data["tracks"]["items"]:
-        this_entry = entry
-        this_entry = this_entry.replace("{name}", track["name"])
-        this_entry = this_entry.replace("{url}", track["preview_url"])
-        content += this_entry
-    return content
     
 
 @app.route("/")
@@ -27,7 +13,11 @@ def choose_year():
     page = f.read()
     f.close()
     page = page.replace("{music_printout}", "")
+    page = page.replace("{year}", "1988")
     return page
+
+
+    
     
 @app.route("/top_10_of_the_year", methods=["POST"])
 def music_page():
@@ -39,8 +29,6 @@ def music_page():
     page = page.replace("{music_printout}", print_song(form["year"]))
     return page
     
-    #this isn't getting anything to website
-#if I save  the songs and links to a list, then print that list to a separate template? For example look Day 86 exercise 
 
 app.run(host="0.0.0.0", port=81 )
 
--- music.py
+++ music.py
@@ -1,10 +1,13 @@
 import os
 import requests, json
 from requests.auth import HTTPBasicAuth
+from replit import db
+
+
 
 def asking_for_songs_per_year(year):
 
-#setting authenticators using Replit's secrets and guided authentication steps:
+    #setting authenticators using Replit's secrets and guided authentication steps:
     client_id = os.environ['client_id']
     client_secret = os.environ['client_secret']
     
@@ -15,14 +18,23 @@ def asking_for_songs_per_year(year):
     response = requests.post(url, data=data, auth=auth)
     access_token = response.json()["access_token"]
     
-#if want to search based on artist:
+    #if want to search based on artist:
     #artist = input("Artist: ").lower()
     #artist = artist.replace(" ", "%20")
-
-#for this project we use year to get 10 top songs of that year
-  #  year = input("Year: ")
-
+    
+    #for this project we use year to get 10 top songs of that year
+    #  year = input("Year: ")
+    
+    #offset causes web page to show different songs as same year is requested again. 
+    #BUT code like this is skipping the start of the list(?), so this is commented out for now and web page only shows the first 10 from data
     offset = 0
+    """try:
+        offset = db[year]
+        if offset > 50:
+            db[year] = 0
+        db[year] += 10
+    except:
+        db[year] = 10"""
     
     url = "https://api.spotify.com/v1/search"
     headers = {"Authorization": f"Bearer {access_token}"}
@@ -43,3 +55,23 @@ def asking_for_songs_per_year(year):
     """for track in data["tracks"]["items"]:
         print(track["name"])
         print(track["preview_url"]) """                   
+
+
+    
+
+#below func takes the data from above and put the 3 requested details on a html template that shows on the webpage /top_10_of_the_year
+    
+def print_song(year):
+    page = ""
+    f = open("./templates/template.html", "r")
+    page = f.read()
+    f.close
+    data = asking_for_songs_per_year(year)
+    music_list = ""
+    for track in data["tracks"]["items"]:
+        song = page
+        song = song.replace("{song}", track["name"])
+        song = song.replace("{artist}", track["artists"][0]["name"])
+        song = song.replace("{url}", track["preview_url"])
+        music_list += song
+    return music_list
